a85638242552a28cac69b60fe44b8999229f9321
jaelle3 <51474361+jaelle3@users.noreply.github.com>
2019-06-17 11:28:54 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,33 +1,23 @@
-var m = Math.floor(Math.random()*100)+1;
-
-var j = m%2;
-
-	if (j=0){
-
-	alert(m+"Number is even");
+var rm= Math.floor(Math.random()*100)+1; //Generates  random number
+var j=rm%2; //modulus 
+if (j==0)
+    alert("The number is Even");
 else
-
-	alert(m+"Number is odd");
-}
-
-for(var count=1;count<=10;count++)
+    alert("The number is Odd");
+for (count=1;count<=10;count++)
 {
-
-	let num=prompt("Please enter a number")
-
-	if (num<m)
-
-		alert("The number you have chosen is lower, please try again");
-
-	else if (num>m)
-
-		alert("The number you have chosen is higher, please try again");
-
-	else if (num==m)
-
-		alert("Correct! You WIN!");
-
-	else
-		alert("You LOSE!");
+    let num=prompt("Please enter a number:");
+    if (num>rm){
+        alert("Your number is higher. Make another guess");
+    }
+    if (num<rm){
+        alert("Your number is lower. Make another guess");
+    }
+    if (num==rm){
+        alert("YOU WIN!!");
+        break; 
+    }
+    if (count==10) {
+        alert("YOU Lose!!");
+    }
 }
-
